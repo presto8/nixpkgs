667182706fcc4d6341d81912e5fedb1f697fa468
Preston Hunt <me@prestonhunt.com>
2024-06-13 13:38:16 -0700

    nixos/greenclip: warn and exit if DISPLAY unset

--- nixos/modules/services/misc/greenclip.nix
+++ nixos/modules/services/misc/greenclip.nix
@@ -20,6 +20,13 @@ in {
       after    = [ "graphical-session.target" ];
       serviceConfig = {
         ExecStart = "${cfg.package}/bin/greenclip daemon";
+        preStart = ''
+          if [[ -z "$DISPLAY" ]]; then
+            echo "can't run greenclip without DISPLAY set"
+            echo "if you are using X without a display manager, please see: https://nixos.wiki/wiki/Using_X_without_a_Display_Manager"
+            exit 1
+          fi
+        '';
         Restart = "always";
       };
     };
